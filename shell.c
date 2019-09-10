d3f5b6818ebfe36d56800221076acd353c750bc5
Caio <caio@localhost.localdomain>
2019-09-10 14:54:01 -0300

    cleanup + comments

--- shell.c
+++ shell.c
@@ -5,6 +5,15 @@
 #include <sys/wait.h>
 
 char **get_input(char *input) {
+    /*Receives a string INPUT, which contains the user input, 
+    parses it and returns the COMMAND array, which contains 
+    the command in the first position, NULL in the last used 
+    position, and the arguments passed in the middle positions.
+    
+    Ex: input = "ls -l -a"
+    
+    returns: command = ["ls","-l","-a",NULL, ...]*/
+    
     char **command = malloc(8 * sizeof(char *));
     char *separator = " ";
     char *parsed;
@@ -30,7 +39,7 @@ int main() {
     int stat_loc;
 
     while (1) {
-        /*reads user input*/
+        /*Reads user input*/
         printf("shell> ");
        
         scanf("%[^\n]",input);
@@ -46,14 +55,14 @@ int main() {
             perror("fork failed");
             exit(1);
         } else if (child_pid == 0) {
-            /* Never returns if the call is successful */
             /*If child, call execvp on input*/
+            /* Never returns if the call is successful */ 
             if(execvp(command[0], command) < 0) {
                 perror(command[0]);
                 exit(1);
             }
         } else {
-            /*If parent wait for end of child process*/
+            /*If parent, wait for end of child process*/
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
 
