d097903fee76e044414c3cf6ee69262eeb457170
Caio <caio@localhost.localdomain>
2019-10-03 14:29:57 -0300

    organizing files + minix src files

---
+++ EP1/imprime.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int d;
+    scanf("%d",&d);
+    printf("\n%d\n",d);
+    return 0;
+}
---
+++ EP1/teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+    sleep(20);
+    printf("4");
+    return 0;
+}
