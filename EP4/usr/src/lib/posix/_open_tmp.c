77d98fccfa9e6cb50b97d73facc5771c119c8432
Caio <caio@localhost.localdomain>
2019-12-06 17:59:21 -0300

    header files for open_tmp

--- EP4/usr/src/lib/posix/_open_tmp.c
+++ EP4/usr/src/lib/posix/_open_tmp.c
@@ -1,2 +1,12 @@
 /*########################################*/
+#include <lib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+int open_tmp(char* path, char* mode) {
+    message m;
+
+
+}
 /*########################################*/
