5a854f6d5ca3cf0b053d0dd21c2507bc0ee589d8
Caio <caio@localhost.localdomain>
2019-12-06 19:53:13 -0300

    wrong line

--- EP4/usr/src/lib/posix/_open_tmp.c
+++ EP4/usr/src/lib/posix/_open_tmp.c
@@ -40,7 +40,6 @@ PUBLIC int open_tmp(const char* name, char* mode) {
     if (flags & O_CREAT) {
         m.m1_i1 = strlen(name) + 1;
         m.m1_i2 = flags;
-        m.mode = flags;
         m.m1_i3 = va_arg(argp, _mnx_Mode_t);
         m.m1_p1 = (char *) name;
     } else {
